95202addfdb93b885231ac6284cf324a96224427
James Irwin <jamesirwin@protonmal.com>
2018-04-17 15:57:10 -0500

    make cross-platform and fix lint errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    "extends": "stylelint-config-standard",
-    "rules": {
-        "at-rule-empty-line-before": null
-    }
+	extends: 'stylelint-config-standard',
+	rules: {
+		'at-rule-empty-line-before': null,
+		'at-rule-no-unknown': [
+			true,
+			{
+				ignoreAtRules: ['extends', 'tailwind']
+			}
+		],
+		indentation: 'tab',
+		'length-zero-no-unit': null
+	}
 };
