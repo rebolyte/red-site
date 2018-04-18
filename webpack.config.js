8532488628ddc8c7c79153d0946423326ac063a1
James Irwin <jamesirwin@protonmal.com>
2018-04-17 22:32:35 -0500

    webpacking

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 const webpack = require('webpack');
 const path = require('path');
+const glob = require('glob-all');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PurgeCssPlugin = require('purgecss-webpack-plugin');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const ifProd = (val, alt) => {
+	if (typeof val === 'undefined') {
+		return isProd;
+	}
+	return isProd ? val : alt;
+};
+
+console.log('production?', isProd);
 
 const paths = {
 	CWD: path.resolve(__dirname),
@@ -9,6 +22,14 @@ const paths = {
 	SRC: path.resolve(__dirname, 'src'),
 	JS: path.resolve(__dirname, 'src/js')
 };
+
+// Custom PurgeCSS extractor for Tailwind that allows special characters in class names.
+class TailwindExtractor {
+	static extract(content) {
+		// eslint-disable-next-line no-useless-escape
+		return content.match(/[A-z0-9-:\/]+/g) || [];
+	}
+}
 
 module.exports = {
 	entry: {
@@ -22,7 +43,7 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.tsx?$/,
+				test: /\.ts$/,
 				exclude: /node_modules/,
 				use: ['ts-loader']
 			},
@@ -31,23 +52,46 @@ module.exports = {
 				// Loader chaining works from right to left
 				use: [
 					MiniCssExtractPlugin.loader,
-					{ loader: 'css-loader', options: { importLoaders: 1 } },
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+							minimize: ifProd()
+							// sourceMap: true
+						}
+					},
 					'postcss-loader'
 				]
 			}
 		]
 	},
 	resolve: {
-		extensions: ['.ts', '.tsx', '.js']
+		extensions: ['.ts', '.js']
 	},
 	plugins: [
 		// https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/701
 		new MiniCssExtractPlugin({
 			filename: 'styles/[name].[contenthash:10].css'
 		}),
+		// https://tailwindcss.com/docs/controlling-file-size
+		// https://www.purgecss.com/with-webpack
+		ifProd(
+			new PurgeCssPlugin({
+				paths: glob.sync([
+					path.join(paths.CWD, 'layouts/**/*.html'),
+					path.join(paths.JS, '/**/*.{js,ts,vue}')
+				]),
+				extractors: [
+					{
+						extractor: TailwindExtractor,
+						extensions: ['html', 'js', 'ts', 'vue']
+					}
+				]
+			})
+		),
 		new WebpackAssetsManifest({
 			output: '../data/assetMap.json'
 		}),
 		new webpack.NamedModulesPlugin()
-	]
+	].filter(el => el)
 };
